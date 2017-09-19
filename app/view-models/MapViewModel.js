95653f7c3ef20d2c7774229e59d66a01322d7749
Henry Ollarves <henryollh205@gmail.com>
2017-09-19 12:51:58 +0200

    Adding unnecesary semicolons as commanded by Udacity reviewer:

--- app/view-models/MapViewModel.js
+++ app/view-models/MapViewModel.js
@@ -1,7 +1,5 @@
-"use strict";
-
 // Foursquare's API key
-const FOUR_SQ_CLIENT_ID = "OZVT545TTMVYBYDSVSTVXMA1ND1YNEF4B2HSUR4R41XOHBHO"
+const FOUR_SQ_CLIENT_ID = "OZVT545TTMVYBYDSVSTVXMA1ND1YNEF4B2HSUR4R41XOHBHO";
 
 // Google Maps styles
 var styles = [
@@ -69,7 +67,7 @@ var styles = [
             { lightness: -25 }
         ]
     }
-]
+];
 
 /**
  * Main ViewModel function
@@ -81,19 +79,19 @@ function Map() {
     var self = this,
         map,
         drawingManager,
-        geocoder
+        geocoder;
 
-    self.placeName = ko.observable('')
-    self.showPlaces = ko.observable(false)
-    self.placeAddress = ko.observable('')
-    self.placeLat = ko.observable('')
-    self.placeLng = ko.observable('')
-    self.searchPlace = ko.observable('')
-    self.query = ko.observable('')
-    self.selectedPlace = ko.observable('')
-    self.fq_access_token = ko.observable('')
-    self.fqVenues = ko.observableArray()
-    self.markers = []
+    self.placeName = ko.observable('');
+    self.showPlaces = ko.observable(false);
+    self.placeAddress = ko.observable('');
+    self.placeLat = ko.observable('');
+    self.placeLng = ko.observable('');
+    self.searchPlace = ko.observable('');
+    self.query = ko.observable('');
+    self.selectedPlace = ko.observable('');
+    self.fq_access_token = ko.observable('');
+    self.fqVenues = ko.observableArray();
+    self.markers = [];
 
     //Hardcoded list of my favorite places
     self.db = ko.observableArray([{
@@ -121,20 +119,19 @@ function Map() {
         address: "Plaça Rosa dels Vents, 1, 08039 Barcelona",
         lat: 41.368294,
         lng: 2.190314
-    }])
+    }]);
 
-    self.polygon = null
+    self.polygon = null;
 
     /**
      * Retrieves access token from url after foursquare's authentication
      */
     function getUrlVars() {
-        let hashes = window.location.href.slice().split("#")
+        let hashes = window.location.href.slice().split("#");
         if (hashes[1]) {
-            let access_token = hashes[1].split("=")[1]
-            return access_token
+            let access_token = hashes[1].split("=")[1];
+            return access_token;
         }
-
     }
 
     /**
@@ -142,16 +139,15 @@ function Map() {
      * 
      */
     self.init = () => {
-        let access_token = getUrlVars()
-        console.log(access_token)
+        let access_token = getUrlVars();
         if (access_token) {
-            self.fq_access_token(access_token)
+            self.fq_access_token(access_token);
         }
         map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: 41.378541, lng: 2.164216 },
             zoom: 13,
             styles: styles
-        })
+        });
         drawingManager = new google.maps.drawing.DrawingManager({
             drawingMode: google.maps.drawing.OverlayType.POLYGON,
             drawingControl: true,
@@ -161,18 +157,17 @@ function Map() {
                     google.maps.drawing.OverlayType.POLYGON
                 ]
             }
-        })
+        });
         google.maps.event.addListener(map, 'click', event => {
-            self.placeLat(event.latLng.lat())
-            self.placeLng(event.latLng.lng())
+            self.placeLat(event.latLng.lat());
+            self.placeLng(event.latLng.lng());
             geocoder.geocode({ 'location': { lat: event.latLng.lat(), lng: event.latLng.lng() } }, (result, status) => {
                 if (status === 'OK') {
-                    self.placeAddress(result[0].formatted_address)
+                    self.placeAddress(result[0].formatted_address);
                 }
             })
-
         })
-        geocoder = new google.maps.Geocoder
+        geocoder = new google.maps.Geocoder;
     }
 
     /**
@@ -180,14 +175,14 @@ function Map() {
      * 
      */
     self.textFilter = ko.computed(() => {
-        let search = self.query().toLowerCase()
+        let search = self.query().toLowerCase();
         if (!search) {
-            return self.db()
+            return self.db();
         }
         return ko.utils.arrayFilter(self.db(), place => {
-            return place.name.toLowerCase().indexOf(search) >= 0
+            return place.name.toLowerCase().indexOf(search) >= 0;
         })
-    })
+    });
 
     /**
      * Opens modal with Foursquare venues
@@ -196,10 +191,10 @@ function Map() {
      */
     self.openInstaModal = (loc) => {
         if (!self.fq_access_token()) {
-            window.open("https://foursquare.com/oauth2/authenticate?client_id=" + FOUR_SQ_CLIENT_ID + "&response_type=token&redirect_uri=http://localhost:8080", "_self")
+            window.open("https://foursquare.com/oauth2/authenticate?client_id=" + FOUR_SQ_CLIENT_ID + "&response_type=token&redirect_uri=http://localhost:8080", "_self");
         } else {
-            let ltlng = loc.lat + "," + loc.lng
-            let access_token = self.fq_access_token()
+            let ltlng = loc.lat + "," + loc.lng;
+            let access_token = self.fq_access_token();
             $.ajax({
                 url: "https://api.foursquare.com/v2/venues/search",
                 type: "GET",
@@ -210,10 +205,10 @@ function Map() {
                 },
                 success: (data) => {
                     if (data.response.venues.length > 1) {
-                        self.fqVenues(data.response.venues)
-                        $("#myModal").modal('show')
+                        self.fqVenues(data.response.venues);
+                        $("#myModal").modal('show');
                     } else {
-                        alert("Unable to fetch foursquare data")
+                        alert("Unable to fetch foursquare data");
                     }
                 }
             })
@@ -221,20 +216,20 @@ function Map() {
     }
 
     //Subscribing query input to the text filter
-    self.query.subscribe(self.textFilter)
+    self.query.subscribe(self.textFilter);
 
     /**
      * Opens modal to add new places
      * 
      */
     self.addNewPlace = () => {
-        let form = document.getElementById('new-place-form')
+        let form = document.getElementById('new-place-form');
         if (form.className == 'fade-in') {
-            form.className = 'fade-out'
-            form.style.display = 'none'
+            form.className = 'fade-out';
+            form.style.display = 'none';
         } else {
-            form.className = 'fade-in'
-            form.style.display = 'block'
+            form.className = 'fade-in';
+            form.style.display = 'block';
         }
     }
 
@@ -249,8 +244,8 @@ function Map() {
             lat: self.placeLat(),
             lng: self.placeLng()
         }
-        self.db.push(location)
-        self.createMarkers(self.db())
+        self.db.push(location);
+        self.createMarkers(self.db());
     }
 
     /**
@@ -259,20 +254,19 @@ function Map() {
      * 
      */
     self.createMarkers = (location) => {
-
         let bounds = new google.maps.LatLngBounds(),
             largeInfowindow = new google.maps.InfoWindow(),
             defaultIcon = self.makeMarkerIcon('19cfd6'),
-            selectedIcon = self.makeMarkerIcon('FFFF24')
+            selectedIcon = self.makeMarkerIcon('FFFF24');
 
-        self.showPlaces(true)
+        self.showPlaces(true);
         if (self.markers.length > 0) {
-            self.hideMarkers()
+            self.hideMarkers();
         }
         if (location.length <= 0 || location.length === undefined) {
-            location = []
+            location = [];
             ko.utils.arrayForEach(self.db(), place => {
-                location.push(place)
+                location.push(place);
             })
         }
         location.forEach(place => {
@@ -285,18 +279,18 @@ function Map() {
                 animation: google.maps.Animation.DROP,
                 title: place.name,
                 icon: defaultIcon
-            })
-            self.markers.push(marker)
-            bounds.extend(marker.position)
+            });
+            self.markers.push(marker);
+            bounds.extend(marker.position);
             marker.addListener('click', () => {
-                self.loadInfoWindow(marker, largeInfowindow)
-            })
+                self.loadInfoWindow(marker, largeInfowindow);
+            });
             marker.addListener('mouseover', () => {
-                marker.setIcon(selectedIcon)
-            })
+                marker.setIcon(selectedIcon);
+            });
             marker.addListener('mouseout', () => {
-                marker.setIcon(defaultIcon)
-            })
+                marker.setIcon(defaultIcon);
+            });
         });
     }
 
@@ -306,9 +300,9 @@ function Map() {
      */
     self.hideMarkers = () => {
         self.markers.forEach((marker) => {
-            marker.setMap(null)
-        })
-        self.markers = []
+            marker.setMap(null);
+        });
+        self.markers = [];
     }
 
     /**
@@ -317,29 +311,29 @@ function Map() {
      */
     self.toggleDrawing = () => {
         if (drawingManager.map) {
-            drawingManager.setMap(null)
+            drawingManager.setMap(null);
             if (polygon !== null) {
-                polygon.setMap(null)
+                polygon.setMap(null);
             }
         } else {
-            drawingManager.setMap(map)
+            drawingManager.setMap(map);
         }
-        drawingManager.addListener('overlaycomplete', function (event) {
+        drawingManager.addListener('overlaycomplete', (event) => {
             if (self.polygon) {
-                self.polygon.setMap(null)
-                self.hideMarkers()
+                self.polygon.setMap(null);
+                self.hideMarkers();
             }
-            drawingManager.setDrawingMode(null)
+            drawingManager.setDrawingMode(null);
             // Creating a new editable polygon from the overlay.
-            self.polygon = event.overlay
-            self.polygon.setEditable(true)
+            self.polygon = event.overlay;
+            self.polygon.setEditable(true);
             //Calculating area
-            let area = google.maps.geometry.spherical.computeArea(self.polygon.getPath())
+            let area = google.maps.geometry.spherical.computeArea(self.polygon.getPath());
             // Searching within the polygon.
-            self.searchWithinPolygon()
+            self.searchWithinPolygon();
             // Make sure the search is re-done if the poly is changed.
-            self.polygon.getPath().addListener('set_at', self.searchWithinPolygon)
-            self.polygon.getPath().addListener('insert_at', self.searchWithinPolygon)
+            self.polygon.getPath().addListener('set_at', self.searchWithinPolygon);
+            self.polygon.getPath().addListener('insert_at', self.searchWithinPolygon);
         })
     }
 
@@ -351,9 +345,9 @@ function Map() {
         if (self.markers && self.markers.length > 0) {
             self.markers.forEach(marker => {
                 if (google.maps.geometry.poly.containsLocation(marker.position, self.polygon)) {
-                    marker.setMap(map)
+                    marker.setMap(map);
                 } else {
-                    marker.setMap(null)
+                    marker.setMap(null);
                 }
             })
         }
@@ -363,23 +357,22 @@ function Map() {
      * Loads Google Maps InfoWindow
      */
     self.loadInfoWindow = (marker, infowindow) => {
-
         if (infowindow.marker != marker) {
-            infowindow.setContent('')
-            infowindow.marker = marker
+            infowindow.setContent('');
+            infowindow.marker = marker;
             infowindow.addListener('closeclick', () => {
-                infowindow.marker = null
+                infowindow.marker = null;
             })
+
             let streetViewService = new google.maps.StreetViewService(),
-                radius = 50
+                radius = 50;
             function getStreetView(data, status) {
                 if (status == google.maps.StreetViewStatus.OK) {
-                    console.log("Ok!")
                     let nearStreetViewLocation = data.location.latLng,
                         heading = google.maps.geometry.spherical.computeHeading(
-                            nearStreetViewLocation, marker.position)
+                            nearStreetViewLocation, marker.position);
 
-                    infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>')
+                    infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
                     let panoramaOptions = {
                         position: nearStreetViewLocation,
                         pov: {
@@ -394,7 +387,7 @@ function Map() {
                         '<div>No Street View Found</div>');
                 }
             }
-            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView)
+            streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
             infowindow.open(map, marker);
         }
     }
@@ -409,16 +402,15 @@ function Map() {
             new google.maps.Size(21, 34),
             new google.maps.Point(0, 0),
             new google.maps.Point(10, 34),
-            new google.maps.Size(21, 34))
-        return markerImage
+            new google.maps.Size(21, 34));
+        return markerImage;
     }
-
     //Initializing program
-    self.init()
+    self.init();
 }
 
 function initMap() {
-    ko.applyBindings(new Map())
+    ko.applyBindings(new Map());
 }
 
 
